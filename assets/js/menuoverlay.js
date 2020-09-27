71dd7e80d96c5d234f393af65a706c3e9a0347a8
asdub <adam.smitzer@gmail.com>
2020-09-27 20:20:28 +0000

    feat: Added spotify iframe widget

--- assets/js/menuoverlay.js
+++ assets/js/menuoverlay.js
@@ -2,3 +2,10 @@ function menuOverlay() {
    var overlay = document.getElementById('menuoverlay') ;
    overlay.classList.toggle('nav-menu-overlay-display');
 }
+
+function spotifyOverlay() {
+   var spotify = document.getElementById('spotify'),
+        headphones = document.getElementById('headphones') ;
+   spotify.classList.toggle('spotify-display');
+   headphones.classList.toggle('spotify-headphones-active');
+}
