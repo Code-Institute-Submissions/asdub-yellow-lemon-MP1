ea1586dbeb74d0d804ce158d3012e7f718dd0108
asdub <adam.smitzer@gmail.com>
2020-09-28 17:31:50 +0000

    fix: Issue with Spotify widget scaling and position. Also changed location and size of widget

--- assets/js/menubar.js
+++ assets/js/menubar.js
@@ -1,22 +1,25 @@
 window.onscroll = function() {
     //console.log(window.pageYOffset);
     var nav = document.getElementById('navbar'),
-        menu = document.getElementById('mobilemenu');
+        menu = document.getElementById('mobilemenu'),
+        spotifyOverlay = document.getElementById('spotify');
     if ( window.pageYOffset > 40 ) {
         nav.style.transition = "all 0.2s ease";
         nav.style.height = "75px";
         nav.style.fontSize = "75%";
-        menu.style.width = "12vw";
+        menu.style.width = "50px";
+        spotifyOverlay.style.top = "85px";
         menu.style.transition = "all 0.2s ease";    
     } else {
         nav.style.transition = "all 0.2s ease";
         nav.style.height = "100px";
         nav.style.fontSize = "100%";
-        menu.style.width = "15vw";
+        menu.style.width = "70px";
+        spotifyOverlay.style.top = "100px";
         menu.style.transition = "all 0.2s ease";
     }
     
 }
 
 // Credit: http://jsfiddle.net/cgdoh1bo/1 // 
-// Edited to direcdtly change the #navar style rather than apply a class. Alto added a transition //
+// Edited to directly change the #mobilemenu style rather than apply a class //
