e5654f460cc000f46159557a60882581defcfaf4
asdub <adam.smitzer@gmail.com>
2020-11-15 21:20:29 +0000

    Update to main layout, menu fixes & content addded
    fix: Issues with use of Sticky position on header/ mobile nav, style: Layout and styling improvments to homepage, content: Additional Yellow Lemon content added

--- assets/js/menubar.js
+++ assets/js/menubar.js
@@ -1,25 +1,26 @@
 window.onscroll = function() {
-    //console.log(window.pageYOffset);
-    var nav = document.getElementById('navbar'),
-        menu = document.getElementById('mobilemenu'),
-        spotifyOverlay = document.getElementById('spotify');
-    if ( window.pageYOffset > 40 ) {
-        nav.style.transition = "all 0.2s ease";
-        nav.style.height = "75px";
-        nav.style.fontSize = "75%";
-        menu.style.width = "50px";
-        spotifyOverlay.style.top = "85px";
-        menu.style.transition = "all 0.2s ease";    
-    } else {
-        nav.style.transition = "all 0.2s ease";
-        nav.style.height = "100px";
-        nav.style.fontSize = "100%";
-        menu.style.width = "70px";
-        spotifyOverlay.style.top = "100px";
-        menu.style.transition = "all 0.2s ease";
+    if ( document.readyState == ("complete") ) {
+        //console.log(window.pageYOffset);
+        var nav = document.getElementById('navbar'),
+            logo = document.getElementById('navlogo'),
+            menu = document.getElementById('mobilemenu'),
+            spotifyOverlay = document.getElementById('spotify');
+        if ( window.pageYOffset > 50 ) {
+            nav.style.transition = "all 0.2s ease";
+            nav.style.height = "75px";
+            logo.style.fontSize = "75%";
+            menu.style.width = "50px";
+            spotifyOverlay.style.top = "calc(76px + var(--theme-margin))";
+            menu.style.transition = "all 0.2s ease";    
+        } else {
+            nav.style.transition = "all 0.2s ease";
+            nav.style.height = "100px";
+            logo.style.fontSize = "100%";
+            menu.style.width = "70px";
+            spotifyOverlay.style.top = "calc(100px + var(--theme-margin))";
+            menu.style.transition = "all 0.2s ease";
+        }
     }
-    
 }
 
 // Credit: http://jsfiddle.net/cgdoh1bo/1 // 
-// Edited to directly change the #mobilemenu style rather than apply a class //
