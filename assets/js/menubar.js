55d826a70ba8f5c951bd6ca04a9ced278a27d442
asdub <adam.smitzer@gmail.com>
2020-11-16 02:35:13 +0000

    style: Yellow Lemon home layout and styling complete
    fix: Issue with Spotify overlay height on menu height change, style: Completed Events, Follow & Footer sections

--- assets/js/menubar.js
+++ assets/js/menubar.js
@@ -4,19 +4,22 @@ window.onscroll = function() {
         var nav = document.getElementById('navbar'),
             logo = document.getElementById('navlogo'),
             menu = document.getElementById('mobilemenu'),
+            overlay = document.getElementById('menuoverlay'),
             spotifyOverlay = document.getElementById('spotify');
         if ( window.pageYOffset > 50 ) {
             nav.style.transition = "all 0.2s ease";
             nav.style.height = "75px";
             logo.style.fontSize = "75%";
             menu.style.width = "50px";
+            overlay.style.top = "calc(76px + var(--theme-margin))";
             spotifyOverlay.style.top = "calc(76px + var(--theme-margin))";
             menu.style.transition = "all 0.2s ease";    
         } else {
             nav.style.transition = "all 0.2s ease";
             nav.style.height = "100px";
             logo.style.fontSize = "100%";
-            menu.style.width = "70px";
+            menu.style.width = "55px";
+            overlay.style.top = "calc(100px + var(--theme-margin))";
             spotifyOverlay.style.top = "calc(100px + var(--theme-margin))";
             menu.style.transition = "all 0.2s ease";
         }
