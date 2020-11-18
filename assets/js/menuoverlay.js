c9057b78b74eb1c4f210a51ff28a2516fea5a9dd
asdub <adam.smitzer@gmail.com>
2020-11-18 12:28:36 +0000

    fix:Issue with menu overlay JS not closing
    style:Further additions to website styling
    
    feat: Sessions.html & band.html completed
    
    added: Initial news article pages(to be styled)

--- assets/js/menuoverlay.js
+++ assets/js/menuoverlay.js
@@ -1,27 +1,41 @@
+
+var overlay = document.getElementById('menuoverlay'),
+    underlay = document.getElementById('menuunderlay'),
+    menu = document.getElementById('mobilemenu'),
+    menuitem = document.querySelector('#menuoverlay > ul.nav-items > li > a.nav-active')
+    spotify = document.getElementById('spotify'),
+    headphones = document.getElementById('headphones');
+
+
 function menuOverlay() {
-   var overlay = document.getElementById('menuoverlay'),
-        underlay = document.getElementById('menuunderlay'),
-        menu = document.getElementById('mobilemenu');
    overlay.classList.toggle('nav-active');
    underlay.classList.toggle('nav-active');
+   document.querySelectorAll('#menuoverlay > ul.nav-items > li a')
+   .forEach(function(a) {
+        a.addEventListener('click', closeOverlay);
+    });
+}
+//Credit: https://stackoverflow.com/a/48217369 //
+
+function closeOverlay(event) {
+    //document.querySelector(event.path[3]).classList.add('nav-current');
+        underlay.classList.toggle('nav-active');
+        overlay.classList.toggle('nav-active');
+        menu.classList.remove('opened');
+        menuitem.classList.remove('nav-active');
+        //menucurrent.classList.add('nav-active');
+        console.log(event.path[0])
+        console.log(event.target.nodeName)
+        console.log(event.target.path)
 }
 
 function menuUnderlay() {
-   var overlay = document.getElementById('menuoverlay'),
-        underlay = document.getElementById('menuunderlay'),
-        menu = document.getElementById('mobilemenu');
    overlay.classList.toggle('nav-active');
    underlay.classList.toggle('nav-active');
    menu.classList.remove('opened');
 }
 
-
-
-  
-
 function spotifyOverlay() {
-   var spotify = document.getElementById('spotify'),
-        headphones = document.getElementById('headphones') ;
    spotify.classList.toggle('spotify-display');
    headphones.classList.toggle('spotify-headphones-active');
 }
